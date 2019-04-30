93bf5955d30f78e9c71ab65bba1bcedd7bd0816b
shajuk <shaju.shajuk@gmail.com>
2019-04-30 09:10:00 +0530

    First commit

---
+++ src/main/java/com/prediction/app/CricPredictionAppApplication.java
@@ -0,0 +1,15 @@
+package com.prediction.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.prediction.app")
+public class CricPredictionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricPredictionAppApplication.class, args);
+	}
+
+}
